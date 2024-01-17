eb7037226edb5b0549aaa79e44bbee4f642f8b9e
wangliqiang <2423594666@qq.com>
2024-01-17 16:05:46 +0800

    add GradientIndicatorText

--- app/src/main/java/com/app/codeadvance/MainActivity.kt
+++ app/src/main/java/com/app/codeadvance/MainActivity.kt
@@ -2,12 +2,10 @@ package com.app.codeadvance
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.app.codeadvance.databinding.ActivityMainBinding
 import com.app.codeadvance.flowlayout.FlowLayoutActivity
 import com.app.codeadvance.indicator.GradientIndicatorActivity
-import com.google.gson.Gson
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -18,18 +16,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         clickEvent()
-        jsonFormat()
-    }
-
-    private fun jsonFormat() {
-        val jsonStr =
-            "{\"merSn\":\"15133613919\",\"orderId\":\"T401161706164776309\",\"serialNumber\":\"26382484\",\"merOrderNo\":\"9824011695976281\",\"totalAmount\":0.01,\"refundTotalAmount\":0.00,\"orderStatus\":5,\"OrderStatusCn\":\"成功\"}"
-
-        val gson = Gson()
-        val itemData = gson.fromJson(jsonStr,ItemData::class.java)
-        val str = gson.toJson(itemData)
-        Log.e("JSON", "jsonFormat: $str")
-
     }
 
     private fun clickEvent() {
@@ -41,14 +27,3 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
-
-data class ItemData(
-    val OrderStatusCn: String,
-    val merOrderNo: String,
-    val merSn: String,
-    val orderId: String,
-    val orderStatus: Int,
-    val refundTotalAmount: Number,
-    val serialNumber: String,
-    val totalAmount: Number
-)

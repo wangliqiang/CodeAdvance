28dd95aaaa128e6d8488bc0ad0ac3104c70d7359
wangliqiang <2423594666@qq.com>
2024-01-17 16:05:16 +0800

    add GradientIndicatorText

--- app/src/main/java/com/app/codeadvance/MainActivity.kt
+++ app/src/main/java/com/app/codeadvance/MainActivity.kt
@@ -2,9 +2,12 @@ package com.app.codeadvance
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.app.codeadvance.databinding.ActivityMainBinding
 import com.app.codeadvance.flowlayout.FlowLayoutActivity
+import com.app.codeadvance.indicator.GradientIndicatorActivity
+import com.google.gson.Gson
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -15,11 +18,37 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         clickEvent()
+        jsonFormat()
+    }
+
+    private fun jsonFormat() {
+        val jsonStr =
+            "{\"merSn\":\"15133613919\",\"orderId\":\"T401161706164776309\",\"serialNumber\":\"26382484\",\"merOrderNo\":\"9824011695976281\",\"totalAmount\":0.01,\"refundTotalAmount\":0.00,\"orderStatus\":5,\"OrderStatusCn\":\"成功\"}"
+
+        val gson = Gson()
+        val itemData = gson.fromJson(jsonStr,ItemData::class.java)
+        val str = gson.toJson(itemData)
+        Log.e("JSON", "jsonFormat: $str")
+
     }
 
     private fun clickEvent() {
         binding.flowBtn.setOnClickListener {
             startActivity(Intent(this, FlowLayoutActivity::class.java))
         }
+        binding.customIndicatorBtn.setOnClickListener {
+            startActivity(Intent(this, GradientIndicatorActivity::class.java))
+        }
     }
 }
+
+data class ItemData(
+    val OrderStatusCn: String,
+    val merOrderNo: String,
+    val merSn: String,
+    val orderId: String,
+    val orderStatus: Int,
+    val refundTotalAmount: Number,
+    val serialNumber: String,
+    val totalAmount: Number
+)
---
+++ app/src/main/java/com/app/codeadvance/widget/GradientIndicator.java
@@ -0,0 +1,89 @@
+package com.app.codeadvance.widget;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatTextView;
+
+public class GradientIndicator extends AppCompatTextView {
+
+    Paint paint = new Paint();
+    private float mProgress = 0f;
+
+    public GradientIndicator(@NonNull Context context) {
+        super(context);
+    }
+
+    public GradientIndicator(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public GradientIndicator(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public void setProgress(float progress) {
+        this.mProgress = progress;
+        invalidate();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+//        drawCenterLineX(canvas);
+//        drawCenterLineY(canvas);
+        drawTextBlack(canvas);
+        drawTextRed(canvas);
+
+    }
+
+    private void drawTextBlack(Canvas canvas) {
+        Paint.FontMetrics fontMetrics = paint.getFontMetrics();
+        paint.setColor(Color.BLACK);
+        paint.setTextSize(60);
+        float width = paint.measureText("hello world!");
+        float baseLine = (float) getHeight() / 2 + ((fontMetrics.descent - fontMetrics.ascent) / 2 - fontMetrics.descent);
+        float x = (float) getWidth() / 2 - width / 2;
+
+        canvas.drawText("hello world!", x, baseLine, paint);
+    }
+
+    private void drawTextRed(Canvas canvas) {
+        canvas.save();
+        Paint.FontMetrics fontMetrics = paint.getFontMetrics();
+        paint.setColor(Color.RED);
+        paint.setTextSize(60);
+        float width = paint.measureText("hello world!");
+        float baseLine = (float) getHeight() / 2 + ((fontMetrics.descent - fontMetrics.ascent) / 2 - fontMetrics.descent);
+        float x = (float) getWidth() / 2 - width / 2;
+
+        float right = x + width * mProgress;
+        Rect rect = new Rect((int) x, 0, (int) right, getHeight());
+        canvas.clipRect(rect);
+        canvas.drawText("hello world!", x, baseLine, paint);
+        canvas.restore();
+    }
+
+
+    private void drawCenterLineX(Canvas canvas) {
+        canvas.save();
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.RED);
+        paint.setStrokeWidth(3);
+        canvas.drawLine((float) getWidth() / 2, 0, (float) getWidth() / 2, getHeight(), paint);
+    }
+
+    private void drawCenterLineY(Canvas canvas) {
+        canvas.save();
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.RED);
+        paint.setStrokeWidth(3);
+        canvas.drawLine(0, (float) getHeight() / 2, getWidth(), (float) getHeight() / 2, paint);
+    }
+}
